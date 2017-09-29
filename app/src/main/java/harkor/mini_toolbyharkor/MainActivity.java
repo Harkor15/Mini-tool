151323bce62ce659104580c78f232172980bad91
Harkor15 <krzbigos@gmail.com>
2017-09-29 17:53:39 +0200

    First changes... (learning git)

--- app/src/main/java/harkor/mini_toolbyharkor/MainActivity.java
+++ app/src/main/java/harkor/mini_toolbyharkor/MainActivity.java
@@ -1,7 +1,10 @@
 package harkor.mini_toolbyharkor;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void exit(View view){
+    System.exit(0);
+    }
+
+    public void version(View view){
+        Context context=getApplicationContext();
+        Toast.makeText(context,"v0.01",Toast.LENGTH_SHORT).show();
     }
 }
